470d6c7d65f62a49c4d3392d6f67c0b085f8c195
Lusilene Oliveira <lusileneoliveira.altooeste12@gmail.com>
2025-02-09 01:40:50 -0300

    commit 4

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clientes', function () {
+    return view('clientes');
 });
